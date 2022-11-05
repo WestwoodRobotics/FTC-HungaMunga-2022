6510472370726d5472b0ef72b5301ced65055c1c
Vick Nguyen <yummybreakfast46@gmail.com>
2022-11-05 11:32:10 -0500

    Added some comments and rearranged code

--- TeamCode/src/main/java/TeleOpr.java
+++ TeamCode/src/main/java/TeleOpr.java
@@ -14,9 +14,9 @@ public class TeleOpr extends OpMode {
     DcMotor frontLeft = null;
     DcMotor backRight = null;
     DcMotor backLeft = null;
-    DcMotor viperSlide = null;
 
     // Viper Motors
+    DcMotor viperSlide = null;
     double vippow = 0;
 
     //Servo
@@ -26,33 +26,36 @@ public class TeleOpr extends OpMode {
     public void init() {
 
         // Motor initialization
-
+        //Drive Motors
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        viperSlide = hardwareMap.get(DcMotor.class, "viperSlide");
-        claw = hardwareMap.get(Servo.class, "claw");
 
         frontRight.setDirection(DcMotor.Direction.REVERSE);
         frontLeft.setDirection(DcMotor.Direction.FORWARD);
         backRight.setDirection(DcMotor.Direction.REVERSE);
         backLeft.setDirection(DcMotor.Direction.FORWARD);
-        viperSlide.setDirection(DcMotor.Direction.FORWARD);
-        claw.setDirection(Servo.Direction.REVERSE);
 
-        // Sets behavior of motors @ 0 power to be at rest
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        viperSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //Viper Slide Motor
+        viperSlide = hardwareMap.get(DcMotor.class, "viperSlide");
 
+        viperSlide.setDirection(DcMotor.Direction.FORWARD);
+
+        viperSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         viperSlide.setMode(DcMotor.RunMode.RESET_ENCODERS);
 
         viperSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        //Servo
+        claw = hardwareMap.get(Servo.class, "claw");
+
+        claw.setDirection(Servo.Direction.REVERSE);
 
         telemetry.addData("Status", "initialized");
         telemetry.update();
@@ -65,30 +68,41 @@ public class TeleOpr extends OpMode {
 
     @Override
     public void loop() {
+
+        // - - Gamepad Buttons - - //
+        // Drive buttons (yes, buttons)
         double drive = gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
         double turn = gamepad1.right_stick_x;
 
+        // Viper buttons
+        boolean high = gamepad1.y;
+        boolean medium = gamepad1.x;
+        boolean low = gamepad1.a;
+        boolean base = gamepad1.b;
+
+        boolean vipup = gamepad1.right_bumper;
+        float vipdow = gamepad1.right_trigger;
+
+        // Servo buttons
+        boolean clawOpen = gamepad1.dpad_left;
+        boolean clawClose = gamepad1.dpad_right;
+
+        //Drive Power
         double frontRightPower = drive - strafe + turn;
         double frontLeftPower = drive + strafe - turn;
         double backRightPower = drive + strafe + turn;
         double backLeftPower = drive - strafe - turn;
 
-        boolean high = gamepad1.y;
-        boolean medium = gamepad1.x;
-        boolean low = gamepad1.a;
-        boolean base = gamepad1.b;
-        boolean vipup = gamepad1.right_bumper;
-        float vipdow = gamepad1.right_trigger;
+        // Viper slide variables
         double height = viperSlide.getCurrentPosition();
         double targetPos = 0;
 
+        // Servo Position variable
         double clawPos = 0;
 
-        boolean clawOpen = gamepad1.dpad_left;
-        boolean clawClose = gamepad1.dpad_right;
 
-
+        // Viper Slide button functions
         if (high && !(medium == low == base) ) {
             targetPos = 1800;
         } else if (medium && !(high == low == base) ) {
@@ -105,6 +119,7 @@ public class TeleOpr extends OpMode {
             vippow = -1;
         }
 
+        // Set Power based on Current position V.S. targEt position
         if (height < targetPos) {
             vippow = 1;
         } else if (height > targetPos) {
@@ -113,25 +128,27 @@ public class TeleOpr extends OpMode {
             vippow = 0;
         }
 
+        // Servo code
         if (clawOpen && !clawClose && clawPos < 1) {
             clawPos += 0.02;
         } else if (clawClose && !clawOpen && clawPos > -1) {
             clawPos -= 0.02;
         }
 
+        // Drive power
         frontRight.setPower(frontRightPower);
         frontLeft.setPower(frontLeftPower);
         backRight.setPower(backRightPower);
         backLeft.setPower(backLeftPower);
 
+        // Viper Slide directions
         viperSlide.setTargetPosition((int)targetPos);
         viperSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         viperSlide.setPower(vippow);
 
-
+        //Servo movement
         claw.setPosition(clawPos);
 
-        telemetry.addData("Value", vippow);
         telemetry.addData("Status", "Running");
         telemetry.update();
     }
