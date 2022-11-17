ce7eceea8abb38e1c6052660e71601fc3ab882b6
Vick Nguyen <yummybreakfast46@gmail.com>
2022-11-17 08:24:33 -0600

    added working teleop

--- TeamCode/src/main/java/TeleOpr.java
+++ TeamCode/src/main/java/TeleOpr.java
@@ -1,32 +1,59 @@
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name = "TeleOpr", group = "Iterative Opmode")
+@TeleOp(name = "HMOpCode",
+        group = "Iterative Opmode")
 public class TeleOpr extends OpMode {
 
+    // Run time instantiation
     private ElapsedTime runtime = new ElapsedTime();
 
-    // Drivetrain Motors
-    DcMotor frontRight = null;
-    DcMotor frontLeft = null;
-    DcMotor backRight = null;
-    DcMotor backLeft = null;
+    //Distance Sensor
+    DistanceSensor distance;
+
+    // Motor Initialization & Instantiation to null
+    DcMotor frontLeft,
+            frontRight,
+            backLeft,
+            backRight,
+            liftMotor = null;
 
     // Viper Motors
     DcMotor viperSlide = null;
     double vippow = 0;
+    double targetPos = 0;
 
-    //Servo
+    // Sero Initialization & Instantiation to null
     Servo claw = null;
+
+    // Gamepad Connections
+
+    float drive = 0;
+    float strafe = 0;
+    float turn = 0;
+    float modifier = 0.5f;
+    double clawPos = 1;
+
+    // Motor Power
+    double frontRightPower;
+    double frontLeftPower;
+    double backRightPower;
+    double backLeftPower;
 
     @Override
     public void init() {
 
-        // Motor initialization
+        // Motor instantiation to hardwareMap
+        //Distance Sensor
+        distance = hardwareMap.get(DistanceSensor.class, "distanceSensor");
+
         //Drive Motors
+
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         backRight = hardwareMap.get(DcMotor.class, "backRight");
@@ -42,6 +69,7 @@ public class TeleOpr extends OpMode {
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        // Lift motor to hardwareMap
         //Viper Slide Motor
         viperSlide = hardwareMap.get(DcMotor.class, "viperSlide");
 
@@ -52,28 +80,21 @@ public class TeleOpr extends OpMode {
 
         viperSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Servo
+        // Servo to hardwareMap
         claw = hardwareMap.get(Servo.class, "claw");
 
-        claw.setDirection(Servo.Direction.REVERSE);
-
-        telemetry.addData("Status", "initialized");
-        telemetry.update();
     }
 
     @Override
-    public void start() {
-        runtime.reset();
-    }
+    public void start() {runtime.reset();}
 
     @Override
+
     public void loop() {
 
-        // - - Gamepad Buttons - - //
-        // Drive buttons (yes, buttons)
-        double drive = gamepad1.left_stick_y;
-        double strafe = gamepad1.left_stick_x;
-        double turn = gamepad1.right_stick_x;
+        drive = gamepad1.left_stick_y;
+        strafe = gamepad1.left_stick_x;
+        turn = gamepad1.right_stick_x;
 
         // Viper buttons
         boolean high = gamepad1.y;
@@ -81,74 +102,89 @@ public class TeleOpr extends OpMode {
         boolean low = gamepad1.a;
         boolean base = gamepad1.b;
 
-        boolean vipup = gamepad1.right_bumper;
-        float vipdow = gamepad1.right_trigger;
+        boolean stack5 = gamepad1.dpad_up;
+        boolean stack4 = gamepad1.dpad_left;
+        boolean stack3 = gamepad1.dpad_right;
+        boolean stack2 = gamepad1.dpad_down;
 
-        // Servo buttons
-        boolean clawOpen = gamepad1.dpad_left;
-        boolean clawClose = gamepad1.dpad_right;
-
-        //Drive Power
-        double frontRightPower = drive - strafe + turn;
-        double frontLeftPower = drive + strafe - turn;
-        double backRightPower = drive + strafe + turn;
-        double backLeftPower = drive - strafe - turn;
+        frontRightPower = drive + strafe + turn;
+        frontLeftPower = drive + strafe - turn;
+        backRightPower = drive - strafe + turn;
+        backLeftPower = drive - strafe - turn;
 
         // Viper slide variables
         double height = viperSlide.getCurrentPosition();
-        double targetPos = 0;
-
-        // Servo Position variable
-        double clawPos = 0;
-
 
         // Viper Slide button functions
         if (high && !(medium == low == base) ) {
-            targetPos = 1800;
+            targetPos = 3829;
         } else if (medium && !(high == low == base) ) {
-            targetPos = 1000;
+            targetPos = 2772;
         } else if (low && !(high == medium == base) ) {
-            targetPos = 200;
+            targetPos = 1691;
         } else if (base && !(high == medium == low) ) {
-            targetPos = 10;
+            targetPos = 60;
+            claw.setPosition(0.32);
+        } else if (gamepad1.dpad_up) {
+            targetPos = 646;
+        } else if (gamepad1.dpad_left) {
+            targetPos = 448;
+        } else if (gamepad1.dpad_right) {
+            targetPos = 323;
+        } else if (gamepad1.dpad_down) {
+            targetPos = 200;
         }
 
-        if (vipup && vipdow == 0 && height < 2730) {
-            vippow = 1;
-        } else if (vipdow > 0 && !vipup && height > 0) {
-            vippow = -1;
+        if (height > 3000) {
+            modifier = 0.3f;
+        } else {
+            modifier = 0.5f;
         }
 
         // Set Power based on Current position V.S. targEt position
         if (height < targetPos) {
-            vippow = 1;
+            vippow = 1.2;
         } else if (height > targetPos) {
-            vippow = -1;
+            vippow = -1.2;
         } else if (height == targetPos) {
             vippow = 0;
         }
 
-        // Servo code
-        if (clawOpen && !clawClose && clawPos < 1) {
-            clawPos += 0.02;
-        } else if (clawClose && !clawOpen && clawPos > -1) {
-            clawPos -= 0.02;
+        if (gamepad1.right_trigger > 0) {
+            targetPos += 23;
+        } else if (gamepad1.left_trigger > 0 && targetPos > 0) {
+            targetPos -= 23;
         }
 
-        // Drive power
-        frontRight.setPower(frontRightPower);
-        frontLeft.setPower(frontLeftPower);
-        backRight.setPower(backRightPower);
-        backLeft.setPower(backLeftPower);
+        frontRight.setPower(frontRightPower * modifier);
+        frontLeft.setPower(frontLeftPower * modifier);
+        backRight.setPower(backRightPower * modifier);
+        backLeft.setPower(backLeftPower * modifier);
+
+        if (gamepad1.left_bumper) {
+            clawPos = 0.26;
+        } else {
+            clawPos = 1;
+        }
+
+        // Distance Sensor
+
+        //  if ((distance.getDistance(DistanceUnit.CM) < 11) && (height < 80) && !gamepad1.left_bumper) {
+        //      claw.setPosition(1);
+        //      if (distance.getDistance(DistanceUnit.CM) < 4) {
+        //          targetPos = 100;
+        //      }
+        //  }
 
         // Viper Slide directions
         viperSlide.setTargetPosition((int)targetPos);
         viperSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         viperSlide.setPower(vippow);
 
-        //Servo movement
         claw.setPosition(clawPos);
 
+        telemetry.addData("Value", distance.getDistance(DistanceUnit.CM));
+        telemetry.addData("Value", height);
         telemetry.addData("Status", "Running");
         telemetry.update();
     }
